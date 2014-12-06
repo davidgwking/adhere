70dbcbc159fbf34bbc9700982721e8ee65695c9c
David King <davidgwking@gmail.com>
2014-12-06 11:45:33 -0600

    serialize browser tests

--- gulp/tasks/test-browser-remote.js
+++ gulp/tasks/test-browser-remote.js
@@ -1,7 +1,7 @@
 var async = require('async');
 var karma = require('karma').server;
 
-var ieRemote = {
+var launchers = {
   slIE9: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
@@ -19,19 +19,13 @@ var ieRemote = {
     browserName: 'internet explorer',
     platform: 'Windows 7',
     version: '11'
-  }
-};
-
-var chrRemote = {
+  },
   slChrome: {
     base: 'SauceLabs',
     browserName: 'chrome',
     platform: 'Windows 7',
     version: '35'
-  }
-};
-
-var ffRemote = {
+  },
   slFirefox: {
     base: 'SauceLabs',
     browserName: 'firefox',
@@ -41,36 +35,17 @@ var ffRemote = {
 };
 
 module.exports = function (done) {
-  // hack browsers tests so they execute serially in batches
-  // karma-sauce-launcher doesn't have concurrency control
-  async.auto({
-
-    ie: function (next) {
-      karma.start({
-        configFile: __dirname + '/../../karma.conf.js',
-        browsers: Object.keys(ieRemote),
-        customLaunchers: ieRemote,
-        browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
-      }, next);
-    },
-
-    ff: ['ie', function (next) {
-      karma.start({
-        configFile: __dirname + '/../../karma.conf.js',
-        browsers: Object.keys(ffRemote),
-        customLaunchers: ffRemote,
-        browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
-      }, next);
-    }],
-
-    chr: ['ff', function (next) {
-      karma.start({
-        configFile: __dirname + '/../../karma.conf.js',
-        browsers: Object.keys(chrRemote),
-        customLaunchers: chrRemote,
-        browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
-      }, next);
-    }],
-
+  // hack browsers tests so they execute serially
+  // karma-sauce-launcher doesn't have concurrency control and sauce oftens starts queueing
+  async.eachSeries(Object.keys(launchers), 1, function (launcher, next) {
+    var configs = {};
+    configs[launcher] = launchers[launcher];
+    
+    karma.start({
+      configFile: __dirname + '/../../karma.conf.js',
+      browsers: [launcher],
+      customLaunchers: configs,
+      browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
+    }, next);
   }, done);
 };
