61254310dfb59d3debc8cf88fb78b48abd2cadd1
David King <davidgwking@gmail.com>
2014-12-06 11:22:13 -0600

    concurrency hack

--- gulp/tasks/test-browser-remote.js
+++ gulp/tasks/test-browser-remote.js
@@ -1,18 +1,7 @@
+var async = require('async');
 var karma = require('karma').server;
 
-var sauceLabsTests = {
-  slChrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 7',
-    version: '35'
-  },
-  slFirefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 7',
-    version: '30'
-  },
+var ieRemote = {
   slIE9: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
@@ -30,14 +19,58 @@ var sauceLabsTests = {
     browserName: 'internet explorer',
     platform: 'Windows 7',
     version: '11'
-  },
+  }
+};
+
+var chrRemote = {
+  slChrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '35'
+  }
+};
+
+var ffRemote = {
+  slFirefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    platform: 'Windows 7',
+    version: '30'
+  }
 };
 
 module.exports = function (done) {
-  karma.start({
-    configFile: __dirname + '/../../karma.conf.js',
-    browsers: Object.keys(sauceLabsTests),
-    customLaunchers: sauceLabsTests,
-    browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
+  // hack browsers tests so they execute serially in batches
+  // karma-sauce-launcher doesn't have concurrency control
+  async.auto({
+
+    ie: function (next) {
+      karma.start({
+        configFile: __dirname + '/../../karma.conf.js',
+        browsers: Object.keys(ieRemote),
+        customLaunchers: ieRemote,
+        browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
+      }, next);
+    },
+
+    ff: ['ie', function (next) {
+      karma.start({
+        configFile: __dirname + '/../../karma.conf.js',
+        browsers: Object.keys(ffRemote),
+        customLaunchers: ffRemote,
+        browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
+      }, next);
+    }],
+
+    chr: ['ff', function (next) {
+      karma.start({
+        configFile: __dirname + '/../../karma.conf.js',
+        browsers: Object.keys(chrRemote),
+        customLaunchers: chrRemote,
+        browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
+      }, next);
+    }],
+
   }, done);
 };
