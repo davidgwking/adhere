64b21d4b5cacb6775b0be7ea71dc40085e0b0a05
David King <davidgwking@gmail.com>
2014-12-06 22:21:33 -0600

    allow browser test failures

--- gulp/tasks/test-browser-remote.js
+++ gulp/tasks/test-browser-remote.js
@@ -179,25 +179,17 @@ var launchers = {
 module.exports = function (done) {
   // hack browsers tests so they execute serially
   // karma-sauce-launcher doesn't have concurrency control and sauce oftens starts queueing
-  var errors = [];
   async.eachSeries(Object.keys(launchers), function (launcher, next) {
     var configs = {};
     configs[launcher] = launchers[launcher];
 
-    karma.start({
+    var karmaConf = {
       configFile: __dirname + '/../../karma.conf.js',
       browsers: [launcher],
       customLaunchers: configs,
       browserNoActivityTimeout: 1000000 // sauce labs builds can queue up
-    }, function (err) {
-      // hack to make sure that we execute all browser tests
-      if (err) errors.push(err);
-      next();
-    });
-  }, function (err) {
-    if (err) return done(err);
-    // hacky, but is symptomatic of above hacks
-    if (errors.length) return done(errors[1]);
-    return done();
-  });
+    };
+
+    karma.start(karmaConf, function () { next(); });
+  }, done);
 };
